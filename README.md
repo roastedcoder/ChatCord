38d12ef89563ce0749caaa33e97a656a6dc4fd2a
Suraj Kumar <vermasuraj412@gmail.com>
2022-01-24 22:09:52 +0530

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * build using Node.js, Express, Socket.io
 * user can login using a username and by selecting a room.
+* deployed on [heroku](https://pure-wave-21818.herokuapp.com/)
--- server.js
+++ server.js
@@ -70,7 +70,5 @@ io.on('connection', socket => {
 
 
 
-
-
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=> console.log('server live at ' + PORT));
